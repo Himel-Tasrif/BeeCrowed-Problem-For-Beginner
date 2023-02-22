f8552eed92e962e0345927934f6311a177faa6f8
Himel-Tasrif <himel35-1078@diu.edu.bd>
2023-02-22 22:23:00 +0600

    problem(1003)

--- Problem(1001).c
+++ Problem(1001).c
@@ -1,10 +1,5 @@
-#include <stdio.h>
+#include<stdio.h>
 int main()
 {
-    int A,B,X;
-    scanf("%d %d",&A,&B);
-    //A = 10;
-    X = A+B;
-    printf("X = %d\n",X);
-    return 0;
+    printf("himel");
 }
