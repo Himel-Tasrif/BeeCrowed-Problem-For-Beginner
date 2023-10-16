7d6b51496e59b7a0aa783d31a2ec57631901ea8e
Himel-Tasrif <himel35-1078@diu.edu.bd>
2023-10-16 23:19:59 +0600

    problem(1060)

---
+++ problem_1060.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    float a[6];
+    int i,count=0;
+    for(i=0; i<6; i++)
+    {
+        scanf("%f",&a[i]);
+        if(a[i] > 0)
+        count++;
+    }
+
+    printf("%d valores positivos\n",count);
+    /*if(a[i] > 0)
+        count++;*/
+    return 0;
+}
